d726a9e2a9bc8cc10ca88cea04dbccae95dfbdea
irfan <irfanfaraaz03@gmail.com>
2023-03-29 13:49:02 +0530

    first react project

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
+    return (
+        <div >
+            <footer>
+                <p>Copyright © {currentYear}</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
